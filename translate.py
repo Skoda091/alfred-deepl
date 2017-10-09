3030b8f86d925206141ba4c898e72b2a61442131
Adam Skołuda <vexhelel@gmail.com>
2017-10-09 12:13:58 +0200

    Fix encoding problems and remove debugging statements

--- translate.py
+++ translate.py
@@ -12,21 +12,12 @@ def main(wf):
     import requests
     import json    # Get args from Workflow, already in normalized Unicode
 
-    args_wf = wf.args
-    # print("#DUPA_-1")
-    # print(args_wf[0])
-
     parser = argparse.ArgumentParser(description='Translate sentences using the DeepL API.')
     parser.add_argument('-l', '--language', default='PL', dest='lang',
                         help="The language to translate into. Defaults to English.")
     parser.add_argument('text', nargs='+', help="The text to be translated.")
-    # print("#DUPA_0")
     args = parser.parse_args()
-    # print(args)
-    # print("#DUPA_1")
-    text = " ".join(args.text)
-    # print("#DUPA_2")
-    # print(text)
+    text = " ".join(wf.args)
 
     sp = re.compile("([^\.!\?;]+[\.!\?;]*)")
     sentences = [s for s in sp.split(text) if len(s) > 0]
@@ -38,24 +29,14 @@ def main(wf):
                     "source_lang_user_selected": "auto",
                     "target_lang": args.lang},
             "priority": 1}}
-    # print(payload)
     r = requests.post('https://deepl.com/jsonrpc', data=json.dumps(payload))
     translations = json.loads(r.text)['result']['translations']
-    # print(translations)
-
     result = ""
     for translation in translations:
         # print(translation)
         beams = sorted(translation['beams'], key=lambda b: -1 * b['score'])
         for beam in beams:
             wf.add_item(title=wf.decode(beam['postprocessed_sentence']), valid=True, icon=ICON_WEB)
-
-    # wf.add_item(
-    #     'No results found',
-    #     'Select to view google search',
-    #     arg = 'http://www.google.com/',
-    #     valid = True)
-
 
     wf.send_feedback()
 
