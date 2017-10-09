03f936a35cd042cf92e6e76291ad73e5b38eacdb
Adam Skołuda <vexhelel@gmail.com>
2017-10-09 13:01:15 +0200

    Script clean up and set preffered target language

--- translate.py
+++ translate.py
@@ -1,10 +1,36 @@
 #!/usr/bin/python
 # encoding: utf-8
+#
+# Copyright © 2017 Adam Skołuda
+#
+# MIT Licence. See http://opensource.org/licenses/MIT
+#
+# Created on 2017-10-02
+#
+
+"""
+Translate DeepL API
+"""
+
 from __future__ import division, print_function, unicode_literals, absolute_import
 
 import sys
 
 from workflow import Workflow, web, ICON_WEB
+
+UPDATE_SETTINGS = {'github_slug': 'Skoda091/alfred-deepl'}
+
+ICON_PL = ''
+ICON_EN = ''
+
+# Shown in error logs. Users can find help here
+HELP_URL = 'https://github.com/Skoda091/alfred-deepl'
+
+# API endpoint for DeepL translation API
+API_URL = 'https://deepl.com/jsonrpc'
+
+# How long to cache results for
+CACHE_MAX_AGE = 20  # seconds
 
 def main(wf):
     import argparse
@@ -25,15 +51,14 @@ def main(wf):
         "jsonrpc": "2.0", "method": "LMT_handle_jobs", "id": 1,
         "params": {
             "jobs": [{"kind": "default", "raw_en_sentence": s} for s in sentences],
-            "lang": {"user_preferred_langs": ["EN", "DE"],
+            "lang": {"user_preferred_langs": ["EN", "PL"],
                     "source_lang_user_selected": "auto",
                     "target_lang": args.lang},
             "priority": 1}}
-    r = requests.post('https://deepl.com/jsonrpc', data=json.dumps(payload))
+    r = requests.post(API_URL, data=json.dumps(payload))
     translations = json.loads(r.text)['result']['translations']
     result = ""
     for translation in translations:
-        # print(translation)
         beams = sorted(translation['beams'], key=lambda b: -1 * b['score'])
         for beam in beams:
             wf.add_item(title=wf.decode(beam['postprocessed_sentence']), valid=True, icon=ICON_WEB)
