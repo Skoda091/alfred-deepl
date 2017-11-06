f1ec67a7f9ff25b532d0025aeafa7fea1db655cd
Adam Skołuda <vexhelel@gmail.com>
2017-11-06 15:46:08 +0100

    Clean up

---
+++ deepl_available_langs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# encoding: utf-8
+#
+# Copyright © 2017 Adam Skołuda
+#
+# MIT Licence. See http://opensource.org/licenses/MIT
+#
+# Created on 2017-11-06
+#
+
+"""
+Translate DeepL API - available languages
+"""
+
+# AVAILABLE_LANGS = {
+#     'German': {'iso_code': 'DE', 'icon': 'lang_icons/de.png'},
+#     'English': {'iso_code': 'EN', 'icon': 'lang_icons/en.png'},
+#     'French': {'iso_code': 'FR', 'icon': 'lang_icons/fr.png'},
+#     'Spanish': {'iso_code': 'ES', 'icon': 'lang_icons/es.png'},
+#     'Italian': {'iso_code': 'IT', 'icon': 'lang_icons/it.png'},
+#     'Dutch': {'iso_code': 'NL', 'icon': 'lang_icons/nl.png'},
+#     'Polish': {'iso_code': 'PL', 'icon': 'lang_icons/pl.png'}
+# }
+
+AVAILABLE_LANGS = {
+    'DE': {'name': 'German', 'icon': 'lang_icons/de.png'},
+    'EN': {'name': 'English', 'icon': 'lang_icons/en.png'},
+    'FR': {'name': 'French', 'icon': 'lang_icons/fr.png'},
+    'ES': {'name': 'Spanish', 'icon': 'lang_icons/es.png'},
+    'IT': {'name': 'Italian', 'icon': 'lang_icons/it.png'},
+    'NL': {'name': 'Dutch', 'icon': 'lang_icons/nl.png'},
+    'PL': {'name': 'Polish', 'icon': 'lang_icons/pl.png'}
+}
--- deepl_select_target_lang.py
+++ deepl_select_target_lang.py
@@ -14,21 +14,12 @@ Translate DeepL API - select target language
 
 from __future__ import division, print_function, unicode_literals, absolute_import
 import sys
+import deepl_available_langs
 from workflow import Workflow
 
-AVAILABLE_LANGS = {
-    'German': {'iso_code': 'DE', 'icon': 'lang_icons/de.png'},
-    'English': {'iso_code': 'EN', 'icon': 'lang_icons/en.png'},
-    'French': {'iso_code': 'FR', 'icon': 'lang_icons/fr.png'},
-    'Spanish': {'iso_code': 'ES', 'icon': 'lang_icons/es.png'},
-    'Italian': {'iso_code': 'IT', 'icon': 'lang_icons/it.png'},
-    'Dutch': {'iso_code': 'NL', 'icon': 'lang_icons/nl.png'},
-    'Polish': {'iso_code': 'PL', 'icon': 'lang_icons/pl.png'}
-}
-
 def main(wf):
-    for key, value in AVAILABLE_LANGS.iteritems():
-        wf.add_item(title=key, valid=True, icon=value['icon'], arg=value['iso_code'])
+    for key, value in deepl_available_langs.AVAILABLE_LANGS.iteritems():
+        wf.add_item(title=value['name'], valid=True, icon=value['icon'], arg=key)
 
     wf.send_feedback()
 
